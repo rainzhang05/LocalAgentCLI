f8adfe8b40edb5baf4804d84ae5a6c2236c82ed8
Rain Zhang <tza103@sfu.ca>
2026-03-21 04:01:08 -0700

    feat: export runtime helpers

---
+++ localagentcli/runtime/__init__.py
@@ -0,0 +1,15 @@
+"""Runtime services and execution helpers."""
+
+from localagentcli.runtime.core import (
+    RuntimeMessage,
+    RuntimeServices,
+    RuntimeTurn,
+    SessionExecutionRuntime,
+)
+
+__all__ = [
+    "RuntimeMessage",
+    "RuntimeServices",
+    "RuntimeTurn",
+    "SessionExecutionRuntime",
+]

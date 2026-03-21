5f692d819c74de44a49616fdb0dececfca0b9850
Rain Zhang <tza103@sfu.ca>
2026-03-21 14:58:49 -0700

    feat: export runtime protocol types

--- localagentcli/runtime/__init__.py
+++ localagentcli/runtime/__init__.py
@@ -6,10 +6,30 @@ from localagentcli.runtime.core import (
     RuntimeTurn,
     SessionExecutionRuntime,
 )
+from localagentcli.runtime.event_log import SessionEventLog
+from localagentcli.runtime.protocol import (
+    ApprovalDecisionOp,
+    InterruptOp,
+    RuntimeEvent,
+    ShutdownOp,
+    Submission,
+    UserTurnOp,
+    serialize_runtime_value,
+)
+from localagentcli.runtime.session_runtime import SessionRuntime
 
 __all__ = [
+    "ApprovalDecisionOp",
+    "InterruptOp",
+    "RuntimeEvent",
     "RuntimeMessage",
     "RuntimeServices",
     "RuntimeTurn",
+    "SessionEventLog",
     "SessionExecutionRuntime",
+    "SessionRuntime",
+    "ShutdownOp",
+    "Submission",
+    "UserTurnOp",
+    "serialize_runtime_value",
 ]

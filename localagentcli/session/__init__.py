bd55a54530bf3d1d9a57c478813cdc24872788f3
Rain Zhang <tza103@sfu.ca>
2026-03-26 01:05:41 -0700

    refactor: export session memory helpers

--- localagentcli/session/__init__.py
+++ localagentcli/session/__init__.py
@@ -1,6 +1,11 @@
 """Session management subsystem."""
 
 from localagentcli.session.manager import SessionManager
+from localagentcli.session.memory import (
+    LONG_HORIZON_MEMORY_KEY,
+    extract_session_memory_entries,
+    merge_long_horizon_memory,
+)
 from localagentcli.session.replay import ReplayResult, replay_session_from_event_log
 from localagentcli.session.sqlite_store import SqliteSessionStore
 from localagentcli.session.state import Message, Session
@@ -8,11 +13,14 @@ from localagentcli.session.store import JsonSessionStore, SessionStore
 
 __all__ = [
     "JsonSessionStore",
+    "LONG_HORIZON_MEMORY_KEY",
     "Message",
     "ReplayResult",
     "Session",
     "SessionManager",
     "SessionStore",
     "SqliteSessionStore",
+    "extract_session_memory_entries",
+    "merge_long_horizon_memory",
     "replay_session_from_event_log",
 ]

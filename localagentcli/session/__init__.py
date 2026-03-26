983371dcf47115a2091b95cb48515039fec90124
Rain Zhang <tza103@sfu.ca>
2026-03-25 23:53:08 -0700

    refactor: export session store and replay APIs

--- localagentcli/session/__init__.py
+++ localagentcli/session/__init__.py
@@ -1,1 +1,18 @@
 """Session management subsystem."""
+
+from localagentcli.session.manager import SessionManager
+from localagentcli.session.replay import ReplayResult, replay_session_from_event_log
+from localagentcli.session.sqlite_store import SqliteSessionStore
+from localagentcli.session.state import Message, Session
+from localagentcli.session.store import JsonSessionStore, SessionStore
+
+__all__ = [
+    "JsonSessionStore",
+    "Message",
+    "ReplayResult",
+    "Session",
+    "SessionManager",
+    "SessionStore",
+    "SqliteSessionStore",
+    "replay_session_from_event_log",
+]

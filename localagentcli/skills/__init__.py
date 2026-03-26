4c52ee9d97b6b0003e543c36b7a0b6413090ced5
Rain Zhang <tza103@sfu.ca>
2026-03-26 03:09:47 -0700

    feat: export skills manager module

---
+++ localagentcli/skills/__init__.py
@@ -0,0 +1,5 @@
+"""Skills runtime helpers."""
+
+from localagentcli.skills.manager import SKILL_FILENAME, SkillDocument, SkillsManager
+
+__all__ = ["SKILL_FILENAME", "SkillDocument", "SkillsManager"]
